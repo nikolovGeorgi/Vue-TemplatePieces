0b48d98535ba126ef2e2691eb9e7c2126f9a55b9
nikolovGeorgi <georgi@gnikolov.ca>
2021-06-19 16:04:36 -0600

    Update Home.vue to show links for Cards and Forms.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: import('@/views/Home.vue'),
+        component: Home,
     },
     {
         path: '/cards',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-	<h1>This is Home page.</h1>
+	<div class="home">
+		<h1 class="title">Select a section in order to view its components.</h1>
+		<div class="views">
+			<router-link to="/cards" class="link">Cards</router-link>
+			<router-link to="/forms" class="link">Forms</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -9,4 +15,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.title {
+	font-style: oblique;
+}
+
+.views {
+	margin-block: 2em;
+	display: flex;
+	justify-content: center;
+}
+
+.link {
+	color: #00ffff;
+	text-decoration: none;
+	font-size: 2rem;
+	font-weight: bold;
+	&:hover {
+		color: #00a2ff;
+	}
+}
+.link + .link {
+	padding-left: 1em;
+}
 </style>
