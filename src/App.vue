6c5bd0d52bf93789d9bf5f33dc9633210126a0f6
nikolovGeorgi <georgi@gnikolov.ca>
2021-06-19 15:45:50 -0600

    Remove App.vue from unseen. Update template to use router-view, and update global css.

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,54 @@
 <template>
-    <CardAnimated />
+	<router-view />
 </template>
 
 <script>
-import CardAnimated from './components/CardAnimated.vue';
-
 export default {
-    name: 'App',
-    components: {
-        CardAnimated,
-    },
+	name: "App",
+	components: {},
 };
 </script>
 
-<style>
-:root {
-    --clr-white: white;
-    --clr-dark: #333;
-    --clr-black: black;
-}
+<style lang="scss">
+$clr-white: white;
+$clr-dark: #333;
+$clr-black: black;
 
 *,
 *::before,
 *::after {
-    box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 /* Initialize Global font size. */
 html {
-    font-size: 16px;
+	font-size: 16px;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+	margin: 0;
+	padding: 0;
 }
 
 /* Set the body for pre-viewing components.  */
 body {
-    display: grid;
-    min-height: 100vh;
-    margin: 0;
-    padding: 0;
-    place-items: center;
-    line-height: 1.5;
-    background: var(--clr-dark);
-    color: var(--clr-white);
+	display: grid;
+	min-height: 100vh;
+	place-items: center;
+	background: $clr-dark;
+	color: $clr-white;
+	margin: 0;
 }
 
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
 }
 </style>
