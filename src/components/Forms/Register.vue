96fa1eee1813b8e8e3229d3bbf7a0ef7f8528b5b
nikolovGeorgi <georgi@gnikolov.ca>
2021-06-23 22:14:51 -0600

    Update template for the register component.

--- src/components/Forms/Register.vue
+++ src/components/Forms/Register.vue
@@ -1,5 +1,55 @@
 <template>
-	<div class="register"></div>
+	<div class="register">
+		<h1>This is Register page.</h1>
+		<form class="form" @submit.prevent="onSubmit">
+			<div class="form-group">
+				<label for="firstName">First Name</label>
+				<input
+					v-model="userData.firstName"
+					type="text"
+					class="form-input"
+					required
+				/>
+			</div>
+			<div class="form-group">
+				<label for="lastName">Last Name</label>
+				<input
+					v-model="userData.lastName"
+					type="text"
+					class="form-input"
+					required
+				/>
+			</div>
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input
+					v-model="userData.username"
+					type="text"
+					class="form-input"
+					required
+				/>
+			</div>
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input
+					v-model="userData.email"
+					type="email"
+					class="form-input"
+					required
+				/>
+			</div>
+			<div class="form-group">
+				<label for="email">Password</label>
+				<input
+					v-model="userData.password"
+					type="password"
+					class="form-input"
+					required
+				/>
+			</div>
+			<button type="submit" class="btn">Register</button>
+		</form>
+	</div>
 </template>
 
 <script>
