fd582b5a415cb75201cf23493cd7fa99756c3f32
nikolovGeorgi <georgi@gnikolov.ca>
2021-06-23 22:09:16 -0600

    Update template for the login component.

--- src/components/Forms/Login.vue
+++ src/components/Forms/Login.vue
@@ -1,5 +1,28 @@
 <template>
-	<div class="login"></div>
+	<div class="login">
+		<h1>This is Login page</h1>
+		<form @submit.prevent="onSubmit">
+			<div class="form-group">
+				<label for="email">Email</label>
+				<input
+					v-model="form.email"
+					type="email"
+					class="form-input"
+					required
+				/>
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input
+					v-model="form.password"
+					type="password"
+					class="form-input"
+					required
+				/>
+			</div>
+			<button type="submit" class="btn">Sign in</button>
+		</form>
+	</div>
 </template>
 
 <script>
